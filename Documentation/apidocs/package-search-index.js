ccf595b96b51dd4c3693ac2cd8746ec583f5ec0b
DanielOcon <116754651+DanielOcon@users.noreply.github.com>
2023-04-28 20:57:34 +0100

    documentation
    adding documentation

---
+++ Documentation/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"danielocon.ocontexteditor"},{"l":"help"},{"l":"themes"}];updateSearchResults();
---
+++ Documentation/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"help","l":"Help"},{"p":"danielocon.ocontexteditor","l":"MainWindow"},{"p":"themes","l":"MiTema"}];updateSearchResults();
